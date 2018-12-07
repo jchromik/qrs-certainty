54cb6d366b6f8cb701683299db233371519c0e42
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-12-07 14:02:59 +0100

    tests and minor fixes for SingleSignalWindowGenerator

--- raccoon/detectors/window_generators.py
+++ raccoon/detectors/window_generators.py
@@ -15,13 +15,21 @@ class SingleSignalWindowGenerator():
             points_to_signal(trigger, len(chunk), detection_size)
             for trigger, chunk in zip(trigger_chunks, signal_chunks)]
 
+    def __check_chunk_index(self, chunk_index):
+        if chunk_index not in range(0, len(self.signal_chunks)):
+            raise IndexError("Chunk index out of bounds.")
+
+    def __check_window_index(self, chunk, window_index):
+        if window_index not in range(0, len(chunk) - self.window_size + 1):
+            raise IndexError("Window index out of bounds.")
+
     def _index_pair(self, window_index):
         for chunk_index, chunk in enumerate(self.signal_chunks):
             usable_chunk_length = len(chunk) - self.window_size
-            if window_index < usable_chunk_length:
+            if window_index <= usable_chunk_length:
                 return chunk_index, window_index
             else:
-                window_index -= usable_chunk_length
+                window_index -= (usable_chunk_length + 1)
         raise IndexError("Window index out of bounds.")
 
     def _index_pairs_for_batch(self, batch_index):
@@ -32,8 +40,13 @@ class SingleSignalWindowGenerator():
             for window_index in range(start, end)]
 
     def _window(self, chunk_index, window_index):
-        start = window_index*self.window_size
-        end = (window_index+1)*self.window_size
+        self.__check_chunk_index(chunk_index)
+        chunk = self.signal_chunks[chunk_index]
+        self.__check_window_index(chunk, window_index)
+
+        start = window_index
+        end = window_index + self.window_size
+        
         return chunk[start:end]
 
     def _windows(self, index_pairs):
@@ -42,6 +55,10 @@ class SingleSignalWindowGenerator():
             for chunk_index, window_index in index_pairs]
 
     def _label(self, chunk_index, window_index):
+        self.__check_chunk_index(chunk_index)
+        chunk = self.signal_chunks[chunk_index]
+        self.__check_window_index(chunk, window_index)
+
         trigger_signal = self.trigger_signals[chunk_index]
         return trigger_signal[window_index + self.window_size // 2]
 
@@ -61,7 +78,10 @@ class SingleSignalWindowGenerator():
         return self._windows(index_pairs), self._labels(index_pairs)
 
     def __getitem__(self, index):
-        return batch(index)
+        return self.batch(index)
 
     def __len__(self):
-        pass
+        num_windows = sum([
+            len(chunk) - self.window_size + 1
+            for chunk in self.signal_chunks])
+        return num_windows // self.batch_size
