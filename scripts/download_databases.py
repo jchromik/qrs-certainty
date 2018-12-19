203f6a290f9452cb8b551f284588ce7006e62af7
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-12-19 14:19:42 +0100

    Rename key for nstdb noises in download_databases.py script.

--- scripts/download_databases.py
+++ scripts/download_databases.py
@@ -33,7 +33,7 @@ databases = {
             "118e00", "118e06", "118e12", "118e18", "118e24", "118e_6",
             "119e00", "119e06", "119e12", "119e18", "119e24", "119e_6"],
         "suffixes": ["atr", "hea", "dat"]},
-    "nstdb-noises": {
+    "noises": {
         "remote": "https://physionet.org/physiobank/database/nstdb",
         "files": ["em", "ma", "bw"],
         "suffixes": ["hea", "dat"]}}
