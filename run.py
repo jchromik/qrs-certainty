e3d5f41fe4a00999bce55830108b690a266bcd49
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-12-27 13:02:29 +0100

    Refactor run.py and add support for mutiple configuration files.

--- run.py
+++ run.py
@@ -1,59 +1,78 @@
 #!/usr/bin/env python3
 
-# Avoid threading issues with Tk, needs to be done before first importing wfdb,
-# since wfdb (somehow) uses matplotlib.
-import matplotlib
-matplotlib.use("agg")
-
-from argparse import ArgumentParser
-from os.path import dirname, isabs, join, realpath
-
 import json
 import os
 import sys
 import warnings
 
-# Parse CLI command
+from argparse import ArgumentParser
+from collections import namedtuple
+from os.path import dirname, join, normpath
+
+# Avoid threading issues with Tk, needs to be done before first importing wfdb,
+# since wfdb (somehow) uses matplotlib.
+import matplotlib
+matplotlib.use("agg")
+
+
+def absify(path, dir):
+    """Convert a path to an absolute path."""
+    return normpath(join(dir, path))
+
+
+def all_records_in(path):
+    """Find all records in a record directory.
+    The given directory must contain no other files!
+    """
+    return list(set([f.split('.')[0] for f in os.listdir(path)]))
+
+
+def load_configuration(conf_path):
+    """Load and rectify a configuration file.
+    Returns a namedtuple to avoid dictionary lookups,
+    hence improving readability.
+    """
+    with open(conf_path, "r") as conf_file:
+        conf_dict = json.load(conf_file)
+    conf_dir = dirname(conf_path)
+    conf_dict["input_dir"] = absify(conf_dict["input_dir"], conf_dir)
+    conf_dict["output_dir"] = absify(conf_dict["output_dir"], conf_dir)
+    if not "records" in conf_dict:
+        conf_dict["records"] = all_records_in(conf_dict["input_dir"])
+    return namedtuple('Configuration', conf_dict.keys())(*conf_dict.values())
+
 
 parser = ArgumentParser(description=(
     "Evaluate different methods for finding QRS complexes "
     "in single channel ECG signals."))
 parser.add_argument(
-    "configuration_file", metavar="CONFIGURATION_FILE", type=str,
+    "configurations", metavar="CONFIGURATION_FILE", type=str, nargs='+',
     help="Configuration file specifying the evaluation.")
 args = parser.parse_args()
 
-def absify(path, dir):
-    return path if isabs(path) else realpath(join(dir, path))
-
-def all_signals_in(path):
-    return list(set([f.split('.')[0] for f in os.listdir(path)]))
-
-# Read configuration file
-
-with open(args.configuration_file, "r") as f:
-    conf = json.load(f)
-    conf["input_dir"] = absify(conf["input_dir"], dirname(f.name))
-    conf["output_dir"] = absify(conf["output_dir"], dirname(f.name))
-    if not "records" in conf:
-        conf["records"] = all_signals_in(conf["input_dir"])
-
-if not conf["verbose"]:
-    # disable Tensorflow logging
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    # disable Python warnings
-    if not sys.warnoptions: warnings.simplefilter("ignore")
-
-# Build Evaluator
 
 sys.path.append(join(dirname(__file__), "./raccoon"))
 from raccoon.utils.builders import evaluator_from_dict
 
-evaluator = evaluator_from_dict(conf)
-cv_method = conf["cv_method"]
-test_records = conf["test_records"] if "test_records" in conf else None
 
-if cv_method == "loocv": evaluator.loocv()
-elif cv_method == "k2": evaluator.kfold(k=2)
-elif cv_method == "k10": evaluator.kfold(k=10)
-elif cv_method == 'defined': evaluator.defined(test_records)
+for configuration in args.configurations:
+
+    conf = load_configuration(configuration)
+
+    if not conf.verbose:
+        # disable Tensorflow logging
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        # disable Python warnings
+        if not sys.warnoptions:
+            warnings.simplefilter("ignore")
+
+    evaluator = evaluator_from_dict(conf._asdict())
+
+    if conf.cv_method == "loocv":
+        evaluator.loocv()
+    elif conf.cv_method == "k2":
+        evaluator.kfold(k=2)
+    elif conf.cv_method == "k10":
+        evaluator.kfold(k=10)
+    elif conf.cv_method == 'defined':
+        evaluator.defined(conf.test_records)
