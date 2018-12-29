b1a951a06f02af9d6918ae771eb0ceaf5d4ccc42
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-12-29 15:19:02 +0100

    Add support for custom annotation file to show_record.py.

--- scripts/show_record.py
+++ scripts/show_record.py
@@ -10,14 +10,19 @@ parser = ArgumentParser(description="A tool for showing ECG recordings.")
 parser.add_argument(
     'name', type=str, help="Name of the record to show."
 )
+parser.add_argument(
+    '-a', '--annotation', type=str, required=False,
+    help="Specify a custom annotation file."
+)
 
 args = parser.parse_args()
 
 record = wfdb.rdrecord(args.name)
 signals = record.p_signal.T
 
+annotation_name = args.name if args.annotation is None else args.annotation
 try:
-    ann = wfdb.rdann(args.name, 'atr')
+    ann = wfdb.rdann(annotation_name, 'atr')
     samples, symbols = ann.sample, ann.symbol
 except FileNotFoundError:
     samples, symbols = [], []
@@ -30,6 +35,7 @@ for ax, signal in zip(axes, signals):
     for symbol in set(symbols):
         x = [samp for samp, sym in zip(samples, symbols) if sym == symbol]
         y = signal[x]
-        ax.scatter(x, y, c='red', marker="${}$".format(symbol), s=100)
+        marker = "${}$".format(symbol) if symbol != '~' else r"$\backsim$"
+        ax.scatter(x, y, c='red', marker=marker, s=100)
 
 plt.show()
