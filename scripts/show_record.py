0af4fa5e0aadac3d20f1475659c0141c9de4116f
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-12-29 14:54:12 +0100

    Show all signals and annotations in show_records.py.

--- scripts/show_record.py
+++ scripts/show_record.py
@@ -3,6 +3,7 @@
 from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
+import numpy as np
 import wfdb
 
 parser = ArgumentParser(description="A tool for showing ECG recordings.")
@@ -13,6 +14,22 @@ parser.add_argument(
 args = parser.parse_args()
 
 record = wfdb.rdrecord(args.name)
+signals = record.p_signal.T
 
-plt.plot(record.p_signal.T[0])
+try:
+    ann = wfdb.rdann(args.name, 'atr')
+    samples, symbols = ann.sample, ann.symbol
+except FileNotFoundError:
+    samples, symbols = [], []
+
+fig, axes = plt.subplots(nrows=len(signals), sharex=True)
+axes = np.array(axes).reshape(-1) # in case len(signal) == 1
+
+for ax, signal in zip(axes, signals):
+    ax.plot(signal)
+    for symbol in set(symbols):
+        x = [samp for samp, sym in zip(samples, symbols) if sym == symbol]
+        y = signal[x]
+        ax.scatter(x, y, c='red', marker="${}$".format(symbol), s=100)
+
 plt.show()
