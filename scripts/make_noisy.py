7bdf8be658abc534aba314fd528774395c956270
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-12-29 20:05:39 +0100

    Add format (fmt) adjustment to make_noisy.py.

--- scripts/make_noisy.py
+++ scripts/make_noisy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+from collections import OrderedDict
 from itertools import product
 from os.path import dirname, splitext
 from shutil import copy
@@ -13,6 +14,20 @@ sys.path.append("../raccoon")
 from raccoon.utils.noiseutils import apply_noise_db
 
 
+# SOURCE:
+# module: wfdb.io == 2.2.0
+# file: _signal.py
+# function: _digi_bounds(fmt)
+# lines: 1416--1439
+FMT_RANGES = OrderedDict([
+    ('80', (-128, 127)),
+    ('212', (-2048, 2047)),
+    ('16', (-32768, 32767)),
+    ('24', (-8388608, 8388607)),
+    ('32', (-2147483648, 2147483647))
+])
+
+
 def noisy_filename(filename, snr):
     name, ext = splitext(filename)
     return "{}e{}{}".format(name, snr, ext)
@@ -20,6 +35,21 @@ def noisy_filename(filename, snr):
 
 def noisy_recordname(recordname, snr):
     return "{}e{}".format(recordname, snr)
+
+
+def adjust_fmt(record):
+    """Adjusts data format for serializing a record.
+    This is necessary because adding noise to the records signals can cause
+    the samples values to be out of the range the format (fmt) can express.
+    A digital signal is expected, i.e. d_signal must be present.
+    """
+    samp_min, samp_max = record.d_signal.min(), record.d_signal.max()
+    for fmt, (fmt_min, fmt_max) in FMT_RANGES.items():
+        if samp_min < fmt_min or samp_max > fmt_max:
+            continue
+        record.fmt = [fmt]*len(record.fmt) if isinstance(record.fmt, list) else fmt
+        return
+    raise RuntimeError("Records samples are too high to be serialized.")
 
 
 parser = ArgumentParser(description="A tool for adding noise to record files.")
@@ -42,8 +72,9 @@ parser.add_argument(
     '-d', '--destination', type=str, required=False, default=dirname(__file__),
     help="Directory to write noisy records to."
 )
-args = parser.parse_args()
 
+args = parser.parse_args()
+args.records = set([splitext(record)[0] for record in args.records])
 noise = wfdb.rdrecord(args.noise)
 
 for record_name, snr in product(args.records, args.snrs):
@@ -52,6 +83,7 @@ for record_name, snr in product(args.records, args.snrs):
     record.record_name = noisy_recordname(record.record_name, snr)
     record.file_name = [noisy_filename(name, snr) for name in record.file_name]
     record.adc(inplace=True)
+    adjust_fmt(record)
     record.wrsamp(write_dir=args.destination)
     if args.annotations:
         copy(
